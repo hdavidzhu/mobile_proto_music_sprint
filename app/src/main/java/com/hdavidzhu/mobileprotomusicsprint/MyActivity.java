b4185fc99e5a3ece78f12e913e4efaf892bea18a
H. David Zhu <huanzhendavidzhu@gmail.com>
2014-10-11 12:15:23 -0400

    Trying to figure out how to commit work to a remote branch.

--- app/src/main/java/com/hdavidzhu/mobileprotomusicsprint/MyActivity.java
+++ app/src/main/java/com/hdavidzhu/mobileprotomusicsprint/MyActivity.java
@@ -4,14 +4,27 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import android.net.Uri;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.widget.ListView;
 
 
 public class MyActivity extends Activity {
+
+    private ArrayList<Song> songList;
+    private ListView songView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        songView = (ListView)findViewById(R.id.song_list);
+        songList = new ArrayList<Song>();
     }
 
 
---
+++ app/src/main/java/com/hdavidzhu/mobileprotomusicsprint/Song.java
@@ -0,0 +1,14 @@
+package com.hdavidzhu.mobileprotomusicsprint;
+
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+}
