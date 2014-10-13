96b5e82ee4428ce4c68e5feb18f5bbe397041744
pmcote <pmcote1@gmail.com>
2014-10-13 13:18:27 -0400

    setting up structures

--- app/src/main/java/com/hdavidzhu/mobileprotomusicsprint/SnapFirebase.java
+++ app/src/main/java/com/hdavidzhu/mobileprotomusicsprint/SnapFirebase.java
@@ -2,18 +2,27 @@ package com.hdavidzhu.mobileprotomusicsprint;
 
 import com.firebase.client.Firebase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by pmc on 10/13/14.
  */
 public class SnapFirebase {
     Firebase snapRef;
 
-    public SnapFirebase {
+    public SnapFirebase(){
          this.snapRef = new Firebase("https://snaptunes.firebaseio.com/");
 
     }
 
     public void postSnap(Song song){
+        Map<String, String> snapMap = new HashMap<String,String>();
+        snapMap.put("user", song.getUser());
+        snapMap.put("metadata", song.getData());
+        snapMap.put("formula", song.getFormula());
+
+        snapRef.push().setValue(snapMap);
 
     }
 }
