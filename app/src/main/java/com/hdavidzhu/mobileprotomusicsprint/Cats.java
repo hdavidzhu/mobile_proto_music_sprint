81ed247267a08930d1b284f02ec4071de7ebdb8c
Casey Alvarado <casey.alvarado@students.olin.edu>
2014-10-13 13:54:44 -0400

    Nothing much has been added. Add into to Song Alterations

---
+++ app/src/main/java/com/hdavidzhu/mobileprotomusicsprint/Cats.java
@@ -0,0 +1,46 @@
+package com.hdavidzhu.mobileprotomusicsprint;
+
+import android.media.SoundPool;
+import android.view.SoundEffectConstants;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.net.rtp.AudioStream;
+import android.provider.MediaStore;
+
+
+import javax.sound.sampled.*;
+//import javax.sound.sampled.Clip;
+//import javax.sound.sampled.AudioFormat;
+//import javax.sound.sampled.AudioInputStream;
+//import javax.sound.sampled.DataLine;
+//import javax.sound.sampled.LineUnavailableException;
+//import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+
+
+import java.io.File;
+import java.io.*;
+import java.net.URL;
+import javax.sound.sampled.*;
+import javax.swing.*;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+/**
+ * Created by casey on 10/13/14.
+ */
+public class Cats {
+
+
+}
